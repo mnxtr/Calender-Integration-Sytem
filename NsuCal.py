f879aafa47eb13c89cba319df72029ce554af9d0
Mansib Newaz <68597019+MansibNewaz@users.noreply.github.com>
2021-03-04 09:00:13 -0800

    Update NsuCal.py

--- NsuCal.py
+++ NsuCal.py
@@ -1,6 +1,6 @@
 import urllib.request
 import pdftables_api
-# fetching the pdf from the nsu site  
+#fetching the pdf from the nsu site  
 pdf_path = "http://www.northsouth.edu/newassets/images/5-240.AcademicCalendarSpring%202021.pdf"
 def download_file(download_url, filename):
     response = urllib.request.urlopen(download_url)    
@@ -9,11 +9,14 @@ def download_file(download_url, filename):
     file.close()
  
 
- #Coverter Api that Converts the given Pdf into Xlxs 
+#Coverter Api that Converts the given Pdf into Xlxs 
 
 download_file(pdf_path, "Test")
 c = pdftables_api.Client('9v1fb38u4aah')
-c.xlsx('Test.pdf', 'output')               #replace c.xlsx with c.csv to convert to CSV
+c.xlsx('Test.pdf', 'output')              
+
+
+#replace c.xlsx with c.csv to convert to CSV
 
 
 
